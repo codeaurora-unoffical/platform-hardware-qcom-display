92f3d7d0955398e8d4565c6816fc866c36015dc2
Archana Sriram <apsrir@codeaurora.org>
2016-03-18 15:30:18 +0530

    libqservice: Defer enabling partial update PP turns off
    When client prefer to enable PU(partial update) over PP(post
    processing), delay enabling PU until PP turns off its
    features completely. This change introduces hand shaking flags
    between PP and HWC through binder calls. When requested to turn
    on PU, we notify PP through the sys_fs node. When PP is done
    shutting off it features, it acknowledges back to HWC to actually
    turn on PU.
    cherry-picked from change-Id: I72008f2313b93497f55c0435367ef62cd772c2c4
    
    Change-Id: I172a51bc91c8679d6be84d677bce6867c1bccb3b

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -2807,7 +2807,6 @@ int MDPComp::setPartialUpdatePref(hwc_context_t *ctx, bool enable) {
         return -1;
     }
     close(fd);
-    sIsPartialUpdateActive = enable;
     return 0;
 }
 }; //namespace
--- libqservice/IQService.h
+++ libqservice/IQService.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
- * Copyright (C) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2012-2014,2016 The Linux Foundation. All rights reserved.
  *
  * Not a Contribution, Apache license notifications and license are
  * retained for attribution purposes only.
@@ -64,6 +64,12 @@ public:
     enum {
         END = 0,
         START,
+    };
+
+    enum {
+        PREF_PARTIAL_UPDATE,
+        PREF_POST_PROCESSING,
+        ENABLE_PARTIAL_UPDATE,
     };
 
     enum {
