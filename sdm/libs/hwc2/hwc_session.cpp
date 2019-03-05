4b8895e9ec12d612994a88f25cdd9ad7a34bbc9b
Subash Chander <subash@codeaurora.org>
2019-03-05 10:33:15 +0530

    hwc2: Validate bounds check for input parcel.
    CRs-Fixed: 2517976
    Change-Id: Iba254b3579cd2c258c3cf80fe234a0de7d75e599

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -985,7 +985,7 @@ android::status_t HWCSession::HandleSetActiveDisplayConfig(const android::Parcel
   int dpy = input_parcel->readInt32();
   int error = android::BAD_VALUE;
 
-  if (dpy > HWC_DISPLAY_VIRTUAL) {
+  if (dpy < HWC_DISPLAY_PRIMARY || dpy > HWC_DISPLAY_VIRTUAL) {
     return android::BAD_VALUE;
   }
 
@@ -1004,7 +1004,7 @@ android::status_t HWCSession::HandleGetActiveDisplayConfig(const android::Parcel
   int dpy = input_parcel->readInt32();
   int error = android::BAD_VALUE;
 
-  if (dpy > HWC_DISPLAY_VIRTUAL) {
+  if (dpy < HWC_DISPLAY_PRIMARY || dpy > HWC_DISPLAY_VIRTUAL) {
     return android::BAD_VALUE;
   }
 
@@ -1024,7 +1024,7 @@ android::status_t HWCSession::HandleGetDisplayConfigCount(const android::Parcel 
   int dpy = input_parcel->readInt32();
   int error = android::BAD_VALUE;
 
-  if (dpy > HWC_DISPLAY_VIRTUAL) {
+  if (dpy < HWC_DISPLAY_PRIMARY || dpy > HWC_DISPLAY_VIRTUAL) {
     return android::BAD_VALUE;
   }
 
