3fb48e0a1cc08dd6f038e88f142372cb0d4164ab
Rajavenu Kyatham <rkyatham@codeaurora.org>
2019-11-05 17:58:17 +0530

    sdm: clear cached geometry on successful prepare
    - multiple prepare rejection happen in doze suspend
      so clear the cached geometry change only on
      successful prepare.
    
    Change-Id: I01ea8761fb8e7139a12748e6c161579e415524ad

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -815,7 +815,6 @@ void HWCDisplay::BuildLayerStack() {
   // TODO(user): Set correctly when SDM supports geometry_changes as bitmask
   layer_stack_.flags.geometry_changed = UINT32((geometry_changes_ ||
                                                 geometry_changes_on_doze_suspend_) > 0);
-  geometry_changes_on_doze_suspend_ = GeometryChanges::kNone;
   // Append client target to the layer stack
   Layer *sdm_client_target = client_target_->GetSDMLayer();
   sdm_client_target->flags.updating = IsLayerUpdating(client_target_);
@@ -1330,6 +1329,9 @@ HWC2::Error HWCDisplay::PrepareLayerStack(uint32_t *out_num_types, uint32_t *out
       callbacks_->Refresh(id_);
       return HWC2::Error::BadDisplay;
     }
+  } else {
+    // clear geometry_changes_on_doze_suspend_ on successful prepare.
+    geometry_changes_on_doze_suspend_ = GeometryChanges::kNone;
   }
 
   for (auto hwc_layer : layer_set_) {
