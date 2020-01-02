8ec044a483c5e5efba3b6e2a61945624bb2da740
Xiaowen Wu <wxiaowen@codeaurora.org>
2020-01-02 11:51:54 -0500

    sdm/hwc2: Fix acquire fd when present failed
    When present failed due to lack of validation, acquire fd should
    not be closed.
    
    Change-Id: Id0ca8420c62fcd43dc17a3c14caebc37d3b793c5

--- sdm/libs/hwc2/hwc_display_virtual.cpp
+++ sdm/libs/hwc2/hwc_display_virtual.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2014-2017, 2019, The Linux Foundation. All rights reserved.
+* Copyright (c) 2014-2017, 2019-2020, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -156,7 +156,9 @@ HWC2::Error HWCDisplayVirtual::Present(int32_t *out_retire_fence) {
       status = HWCDisplay::PostCommitLayerStack(out_retire_fence);
     }
   }
-  CloseAcquireFds();
+  if (status != HWC2::Error::NotValidated) {
+    CloseAcquireFds();
+  }
   close(output_buffer_->acquire_fence_fd);
   return status;
 }
