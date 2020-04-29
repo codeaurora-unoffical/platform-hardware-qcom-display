e45a5c0b8e66fa193b4a780ea505b2b15f765a3f
Hai Li <hali@codeaurora.org>
2020-04-29 11:51:17 -0400

    display: enable GPU composition for protected content on msmnile
    Change-Id: I3c21ab6d4fd1e7ed8e0cba31f64d4ffb21938fc9

--- config/display-product.mk
+++ config/display-product.mk
@@ -103,6 +103,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.wcg_composition_dataspa
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 endif
 
+ifeq ($(TARGET_BOARD_PLATFORM), msmnile)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
+endif
+
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 # Recovery is enabled, logging is enabled
 PRODUCT_PROPERTY_OVERRIDES += \
