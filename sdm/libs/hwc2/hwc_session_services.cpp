a605b7a81c2d6119d8d03fffc289ca7aaa768b8b
Baldev Sahu <bsahu@codeaurora.org>
2020-07-24 10:41:33 +0530

    hwc2: Remove unnecessary lock and redundant disp_id check
    - Remove lock as its not required
    - Remove redundant disp_id validation as its already done
    
    Change-Id: I32ce495868906f7201790d3736b7fd8dbb14191d

--- sdm/libs/hwc2/hwc_session_services.cpp
+++ sdm/libs/hwc2/hwc_session_services.cpp
@@ -586,14 +586,6 @@ Return<bool> HWCSession::isHDRSupported(uint32_t disp_id) {
     return false;
   }
 
-  SCOPE_LOCK(locker_[disp_id]);
-
-  if (is_hdr_display_.size() <= disp_id) {
-    DLOGW("is_hdr_display_ is not initialized for display %d!! Reporting it as HDR not supported",
-          disp_id);
-    return false;
-  }
-
   return static_cast<bool>(is_hdr_display_[disp_id]);
 }
 
